99e4a6111851df5a502c2d33eb8ed219cb5d2076
brench11 <80495641+brench11@users.noreply.github.com>
2021-10-08 12:28:03 +0800

    Create 第一个C++.cpp

---
+++ 第一章/第一个C++.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+int main()
+{
+	//std::cout << "Hello World!" << std::endl;
+	std::cout << "Input two number:" << std::endl;
+	int a, b;
+	std::cin >> a >> b;
+	std::cout << "他们的乘积是：" << a * b << std::endl;
+	return 0;
+}
